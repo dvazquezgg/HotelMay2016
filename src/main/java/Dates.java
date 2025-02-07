0932b27ad991d128557541db4723759954ccbcda
dvazquezgg <35460585+dvazquezgg@users.noreply.github.com>
2025-02-07 11:49:14 -0600

    Code from Paper added to classes

--- src/main/java/Dates.java
+++ src/main/java/Dates.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class Dates
+{ private int day;
+    private int month;
+    private int year;
 
-public class Dates {
+    public Dates(int day, int month, int year)
+    { this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {return day;}
+    public int getMonth() {return month;}
+    public int getYear() {return year;}
+
+    public static int StayDays(Dates x, Dates y)
+    { // method that calculates the number of nights between x and y
+        return 0; //TODO: implement this method
+    }
 }
--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class Group
+{
+    private String name; // name of group
+    private int number; // number of rooms allocated to the group
 
-public class Group {
+    public Group(String name, int number)
+    {
+        this.name = name;
+        this.number = number;
+    }
+    public String getName() {return name;}
+    public int getNumber() {return number;}
+
+    int[] gRooms = new int[number];
+    // array to hold room numbers allocated to the group
+    public double bill(int[] gRooms)
+    {// method that calculates the bill for the group
+        // TODO: implement this method
+        return 0.0;
+    }
 }
